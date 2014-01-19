3455cfadd5a9990e0bfbeb1e26e419961b6c0495
Kevin Johnson <vadskye@gmail.com>
2014-01-19 15:54:48 -0800

    Run test code to output data from Database class
    Also useful example of how argparse works

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import sys
+import classes
+import argparse
+
+def initialize_argument_parser():
+    parser = argparse.ArgumentParser(description='Debugging output')
+    parser.add_argument('-p', '--patient_name', dest='patient_name',
+            type=str, help='The name of a patient', default='John Doe')
+    return vars(parser.parse_args())
 
 if __name__ == '__main__':
+    args = initialize_argument_parser()
     print 'Hello world'
     print 'Your current version is', sys.version
     print 'Your current version should be 2.7.6'
     print "Let's get started, shall we?"
     print "First baby steps in the New World"
+    database = classes.Database()
+    print 'patient info: ', database.get_patient_info(args['patient_name'])
