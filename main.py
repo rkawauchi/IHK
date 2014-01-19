7be3c1c7d1a881683ff8f0a27b6d6dae0a093b20
rkawauchi <RieK@Ries-MacBook-Pro.local>
2014-01-19 10:37:39 -0800

    Revert "Revert "Added version printing""
    This reverts commit 7f392958bc8d4327e0e9d6dc169eee26ea80c279.

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == '__main__':
+    print 'Hello world'
+    print 'Your current version is', sys.version
+    print 'Your current version should be 2.7.6'
+    print "Let's get started, shall we?"
