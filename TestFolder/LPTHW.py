037c61138a4e26d674c085100c7f967767913bc1
Thibault <tduchemin1@gmail.com>
2014-01-23 18:24:38 -0800

    Line added

---
+++ TestFolder/LPTHW.py
@@ -0,0 +1,39 @@
+print "I will now count my chickens:"
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+print "Now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+print "Is this true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh that's why it's False."
+
+print "How about some ore."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+cars = 100.0
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+
+print "There are", cars, "cars available here"
+
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's got %s eyes and %s hair" %(my_eyes, my_hair)
+print "If I add %d, %d, and %d, I get %d" % (my_age, my_height, my_weight, my_age + my_weight + my_height)
+
