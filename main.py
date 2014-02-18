a91bce34f2bc50237eec1771bae7a9258acb80d3
Kevin Johnson <vadskye@gmail.com>
2014-02-18 12:01:17 -0800

    Begin SQLAlchemy: create Solution class

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import sys
-import classes
 import argparse
+import sqlalchemy
 
 def initialize_argument_parser():
     parser = argparse.ArgumentParser(description='Debugging output')
-    parser.add_argument('-p', '--patient_name', dest='patient_name',
-            type=str, help='The name of a patient', default='John Doe')
+    parser.add_argument('-n', '--name', dest='name', type=str,
+            help='The name of the solution')
+    parser.add_argument('--is_for_profit', dest='is_for_profit', type=int,
+            help='1 if for profit, 0 if nonprofit')
     return vars(parser.parse_args())
+
+class Solution(base):
+    __tablename__ = 'solutions'
+    id = sqlalchemy.Column("rowid", sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    is_for_profit = sqlalchemy.Column(sqlalchemy.Integer)
+    
+    def __init__(self, name, is_for_profit):
+        self.name=name
+
+    def __repr__(self):
+        return 'Solution({0}, {1})'.format(self.name, self.is_for_profit)
 
 if __name__ == '__main__':
     args = initialize_argument_parser()
     print 'Hello world'
-    print 'Your current version is', sys.version
-    print 'Your current version should be 2.7.6'
-    print "Let's get started, shall we?"
-    print "First baby steps in the New World"
     print "Emilie"
     print "second test"
     database = classes.Database()
