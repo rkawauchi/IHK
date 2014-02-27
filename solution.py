80529d5032b6728adcaad426310c30b5e6366ad4
Kevin Johnson <vadskye@gmail.com>
2014-02-27 00:31:30 -0800

    Clean up and finish Kiosk class
    There was some redundancy because I merged it poorly

--- solution.py
+++ solution.py
@@ -1,7 +1,12 @@
 class Kiosk():
-    def __init__(self, visit_cost, location):
-        self.visit_cost = visit_cost
+    def __init__(self, location, visit_cost, diabetes_threshold,
+            cardio_threshold):
         self.location = location
+        self.visit_cost = visit_cost
+        self.diabetes_threshold = diabetes_threshold
+        self.cardio_threshold = cardio_threshold
+        #Initial cost to create kiosk: $5000. We are measuring in rupees
+        self.money = -309900
         print 'initializing Kiosk'
 
     #patient shold be Person
@@ -11,20 +16,20 @@ class Kiosk():
             return False
         if not patient.money>self.visit_cost:
             print 'patient cannot afford treatment'
-
-    #patient should be Person
-    def visit(self, patient):
         patient.money -= visit_cost
-        #improve patient.diabetes
-        #improve patient.cardio
-        return True
+        kiosk.money += visit_cost
+        
+        #If we diagnose diabetes
+        if patient.diabetes<diabetes_threshold:
+            #For now, we ignore the details and just improve the patient's
+            #health.
+            patient.diabetes = diabetes_threshold
 
-    #Patient should be from class Person
-    def filter(self, patient):
-        if not patient.location == self.location:
-            print "patient not at proper location"
-            return False
-        if not patient.money>self.visit_cost:
-            print "patient cannot afford treatment"
-            return False
-        visit(self,patient)
+        #If we diagnose cardiovascular problems
+        if patient.cardio<cardio_threshold:
+            #For now, we ignore the details and just improve the patient's
+            #health.
+            patient.cardio = cardio_threshold
+
+        #The visit was successful
+        return True
