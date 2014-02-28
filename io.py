45dc2381a4a7d75ad5002e29514c65ba50bb9dff
Kevin Johnson <vadskye@gmail.com>
2014-02-28 11:09:33 -0800

    create State database class

--- io.py
+++ io.py
@@ -28,6 +28,25 @@ class District(Base):
                 self.classification, self.household_total,
                 self.population_total)
 
+class State(Base):
+    __tablename__ = 'states'
+    id = sqlalchemy.Column('rowid', sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    household_poor = sqlalchemy.Column(sqlachemy.Integer)
+    household_middle = sqlalchemy.Column(sqlachemy.Integer)
+    household_rich = sqlalchemy.Column(sqlachemy.Integer)
+    
+    def __init__(self, name):
+        self.name=name
+        self.household_poor=household_poor
+        self.household_middle=household_middle
+        self.household_rich=household_rich
+
+    def __repr__(self):
+        return 'District({0}, {1}, {2}, {3}, {4})'.format(self.name,
+                self.household_poor, self.household_middle,
+                self.household_rich)
+
 def fetch_session(db_filename = 'database.sqlite3'):
     engine = sqlalchemy.create_engine('sqlite:///{0}'.format(db_filename))
     session = orm.sessionmaker(bind=engine)
