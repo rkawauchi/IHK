ea2bf30629dd7986d0e20041c8633897b2b1a324
Kevin Johnson <vadskye@gmail.com>
2014-03-05 17:30:22 -0800

    Put many demos in demonstrate_queries()

--- main.py
+++ main.py
@@ -9,12 +9,26 @@ def initialize_argument_parser():
             help='the solution to test', default='health kiosk')
     return vars(parser.parse_args())
 
+def demonstrate_queries():
+    
+    mizoram = data.get_state_by_name('Mizoram')
+    karnataka = data.get_state_by_abbreviation('KA')
+
+    print mizoram
+    print karnataka
+
+    mizoram_districts = data.get_districts_by_state_name('Mizoram')
+    same_mizoram_districts = data.get_districts_by_state_name(mizoram.name)
+    also_same_mizoram_districts = data.get_districts_by_state(mizoram)
+    
+    print mizoram_districts
+    print mizoram_districts == also_same_mizoram_districts
+
+    #for state in data.get_all_states():
+    #    print state.to_dict()
+
 if __name__ == "__main__":
     args = initialize_argument_parser()
     data = io.Database()
     
-    print data.get_state_by_name('Mizoram')
-    print data.get_state_by_abbreviation('KA')
-
-    #for state in data.get_all_states():
-    #    print state.to_dict()
+    demonstrate_queries()
