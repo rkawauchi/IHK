62943fba8a34b15dab409ab2beae26e11aceb88c
Kevin Johnson <vadskye@gmail.com>
2014-03-09 10:58:43 -0700

    methods to add people to the database

--- people.py
+++ people.py
@@ -12,6 +12,27 @@ class Person():
     def __str__(self):
         return 'I am a person'
 
+def generate_population(data):
+    for state in data.session.query(io.State):
+        generate_state_population(data, state)
+
+def generate_state_population(data, state):
+    for district in data.get_districts_by_state_name(state.name):
+        generate_district_population(data, state, district)
+
+#Accept district and state objects, plus the Database object from io.py
+def generate_district_population(data, state, district):
+    for i in xrange(district.population_total):
+        person = io.Person(money = 1337, diabetes = 0.1, cardio = 0.2, 
+                district = district.name, state = state.name,
+                classification = 'ignored')
+        insert = io.Person.__table__.insert()
+        data.connection.execute(insert, person.__dict__)
+    #commit the changes; otherwise, they will be wasted!
+    data.session.commit()
+    print 'Population of', district.name, 'inserted'
+    
+
 #THIS METHOD DOES NOT WORK
 #It is kept as a reminder of why we can't generate the entire population at once
 #Accept Database object from io.py as input
