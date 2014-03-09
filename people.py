81d10d5d4bf6a47a20bd6848ea6bd445d8c933d4
Kevin Johnson <vadskye@gmail.com>
2014-03-09 12:01:46 -0700

    Working people generation and database insertion!

--- people.py
+++ people.py
@@ -1,37 +1,42 @@
 import io
 
-class Person():
-    def __init__(self, money, diabetes, cardio, district, state, classification):
-        self.money = money
-        self.diabetes = diabetes
-        self.cardio = cardio
-        self.district = district
-        self.state = state
-        self.classification = classification
-
-    def __str__(self):
-        return 'I am a person'
-
 def generate_population(data):
     for state in data.session.query(io.State):
         generate_state_population(data, state)
 
 def generate_state_population(data, state):
-    for district in data.get_districts_by_state_name(state.name):
+    #get only the "total population" entry for each district
+    for district in data.session.query(io.District).filter(
+            io.District.state == state.name).filter(
+            io.District.classification == 'Total'):
         generate_district_population(data, state, district)
 
 #Accept district and state objects, plus the Database object from io.py
 def generate_district_population(data, state, district):
-    for i in xrange(district.population_total):
-        person = io.Person(money = 1337, diabetes = 0.1, cardio = 0.2, 
-                district = district.name, state = state.name,
-                classification = 'ignored')
-        insert = io.Person.__table__.insert()
-        data.connection.execute(insert, person.__dict__)
+    #Bulk insert as per http://docs.sqlalchemy.org/en/rel_0_8/faq.html
+    #split into multiple insertion waves due to memory limitations
+    insertions_per_wave = 1000000
+    #insert the population in waves of 1000000 people 
+    for i in xrange(district.population_total/insertions_per_wave):
+        insert_wave(data, state, district, insertions_per_wave)
+        print 'wave inserted'
+    #insert the last few people
+    insert_wave(data, state, district, district.population_total % insertions_per_wave)
     #commit the changes; otherwise, they will be wasted!
     data.session.commit()
     print 'Population of', district.name, 'inserted'
-    
+
+def insert_wave(data, state, district, insertion_count):
+    insert = io.Person.__table__.insert()
+    data.engine.execute(insert, [generate_person_dict(data, state, district)
+            for j in xrange(insertion_count)])
+
+#generate a dict of values corresponding to the attributes of a Person
+def generate_person_dict(data, state, district):
+    #This is where math and statistics comes in
+    return {'money': 1337, 'diabetes': 0.1,
+            'cardio': 0.2, 'district': district.name, 'state': state.name,
+            'classification': 'ignored'}
 
 #THIS METHOD DOES NOT WORK
 #It is kept as a reminder of why we can't generate the entire population at once
