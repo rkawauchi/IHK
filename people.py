699c6eb91cc64b7d8b31b99b7fb36a34ccff50a1
Kevin Johnson <vadskye@gmail.com>
2014-03-09 10:34:32 -0700

    generate_people method that doesn't work
    Can't generate whole population at once

--- people.py
+++ people.py
@@ -1,6 +1,27 @@
+import io
+
 class Person():
-    def __init__(self, money, diabetes, cardio, location):
+    def __init__(self, money, diabetes, cardio, district, state, classification):
         self.money = money
         self.diabetes = diabetes
         self.cardio = cardio
-        self.location = location
+        self.district = district
+        self.state = state
+        self.classification = classification
+
+    def __str__(self):
+        return 'I am a person'
+
+#THIS METHOD DOES NOT WORK
+#It is kept as a reminder of why we can't generate the entire population at once
+#Accept Database object from io.py as input
+def generate_people(data):
+    population = list()
+    for state in data.session.query(io.State).all():
+        for district in data.get_districts_by_state_name(state.name):
+            for i in xrange(district.population_total):
+                population.append(Person('money', 'diabetes', 'cardio', 
+                    district.name, state.name, 'ignored'))
+            print 'district', district.name
+        print 'state', state.name
+    return population
