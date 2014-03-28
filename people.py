4b953e8f6b1dca9a63312f13d437e545a3ca68e6
Kevin Johnson <vadskye@gmail.com>
2014-03-28 15:05:42 -0700

    Python uses lower_case_names
    minimize opportunity for confusion

--- people.py
+++ people.py
@@ -37,7 +37,8 @@ def generate_district_population(data, state, district):
 
 def insert_wave(data, state, district, mpce, insertion_count):
     insert = io.Person.__table__.insert()
-    data.engine.execute(insert, [generate_person_dict(data, state, district, mpce)
+    data.engine.execute(insert, 
+            [generate_person_dict(data, state, district, mpce)
             for j in xrange(insertion_count)])
 
 #generate a dict of values corresponding to the attributes of a Person
@@ -64,21 +65,21 @@ def generate_people(data):
 
 ######################## below by RieK #########################
 
-def pop_byState(state_name, class_type):
+def pop_by_state(state_name, class_type):
     return data.session.query(io.District.state, io.District.name, io.District.classification, func.sum(io.District.population_total)).filter(io.District.state == state_name).filter(io.District.classification == class_type).group_by(io.District.state).all()
 
-def exp_byState(state_name, class_type):
+def exp_by_state(state_name, class_type):
     return data.session.query(io.Mpce.state, io.Mpce.mpce_type, io.Mpce.classification, io.Mpce.mpce_average).filter(io.Mpce.state == state_name).filter(io.Mpce.mpce_type == "mmrp").filter(io.Mpce.classification == class_type).all()
 
-def exp_Percentile(state_name, class_type):
+def exp_percentile(state_name, class_type):
     temp = data.session.query(io.Mpce).filter(io.Mpce.state == state_name).filter(io.Mpce.mpce_type == "mmrp").filter(io.Mpce.classification == class_type).first()
     return temp.get_d_all(False)
 
 def generate_expense(state_name, class_type):
-    # mean = exp_byState(state_name, class_type)[0][3]
+    # mean = exp_by_state(state_name, class_type)[0][3]
     # For testing, only generating 100,000th of population
-    pop = 0.1 * (pop_byState(state_name, "Rural")[0][3])/100000
-    listPercentile = exp_Percentile(state_name, class_type)
+    pop = 0.1 * (pop_by_state(state_name, "Rural")[0][3])/100000
+    listPercentile = exp_percentile(state_name, class_type)
     expenseList=[]
     for i in xrange(len(listPercentile)-1):
         genUniform = np.random.uniform(listPercentile[i], listPercentile[i+1], pop)
@@ -87,19 +88,17 @@ def generate_expense(state_name, class_type):
 
 def generate_expense_log(state_name, class_type):
     # For testing, only generating 100,000th of population
-    pop = (pop_byState(state_name, "Rural")[0][3])/100000
-    listPercentile = exp_Percentile(state_name, class_type)
+    pop = (pop_by_state(state_name, "Rural")[0][3])/100000
+    listPercentile = exp_percentile(state_name, class_type)
     logPercentile = []
     # http://stackoverflow.com/questions/4561113/python-list-conversion
     logPercentile[:] = [log(x) for x in listPercentile]
     expenseList = np.random.lognormal(mean=np.mean(logPercentile), sigma=np.std(logPercentile), size=pop)
     return expenseList
 
-
 if __name__ == '__main__':
     data = io.Database()
-    print pop_byState("Tamil Nadu", "Rural")[0][3]
-    print exp_byState("Tamil Nadu", "rural")[0][3]
-    print exp_Percentile("Andhra Pradesh", "rural")
+    print pop_by_state("Tamil Nadu", "Rural")[0][3]
+    print exp_by_state("Tamil Nadu", "rural")[0][3]
+    print exp_percentile("Andhra Pradesh", "rural")
     print generate_expense_log("Andhra Pradesh", "rural")[1:10]
-
