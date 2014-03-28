4b9ab3123c3f14bcab7c1512cde9fc82ba1afad1
Kevin Johnson <vadskye@gmail.com>
2014-03-28 14:49:04 -0700

    import_data set with passed command line arg

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ def initialize_argument_parser():
     parser = argparse.ArgumentParser(description='Simulate Indian health solutions')
     parser.add_argument('-s', '--solution', dest='solution', 
             help='the solution to test', default='health kiosk')
+    parser.add_argument('--import-data', dest='import_data',
+            action='store_true', default=False,
+            help='Recreate database from raw data files')
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
     args = initialize_argument_parser()
-    data = io.Database(import_data=True)
+    data = io.Database(import_data=args['import_data'])
     test_state_name = 'Meghalaya'
     test_state = data.get_state_by_name(test_state_name)
     print 'test_state', test_state
