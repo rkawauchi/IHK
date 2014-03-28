f92ad88d626230a757b3d241462baa9164af4340
Kevin Johnson <vadskye@gmail.com>
2014-03-28 15:29:34 -0700

    remove deprecated method to avoid confusion

--- people.py
+++ people.py
@@ -57,22 +57,6 @@ def generate_person(data, state, district, mpce):
     person = io.Person(money, diabetes, cardio, district.name, state.name,
             classification)
     return person
-    
-
-#THIS METHOD DOES NOT WORK
-#It is kept as a reminder of why we can't generate the entire population at once
-#Accept Database object from io.py as input
-def generate_people(data):
-    population = list()
-    for state in data.session.query(io.State).all():
-        for district in data.get_districts_by_state_name(state.name):
-            for i in xrange(district.population_total):
-                population.append(Person('money', 'diabetes', 'cardio', 
-                    district.name, state.name, 'ignored'))
-            print 'district', district.name
-        print 'state', state.name
-    return population
-
 
 ######################## below by RieK #########################
 
