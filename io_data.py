65d95f20ebcbdd705091f2c6be64379c63d420ea
Kevin Johnson <vadskye@gmail.com>
2014-04-09 10:09:50 -0700

    Change eye_health to health in database

--- io_data.py
+++ io_data.py
@@ -131,19 +131,19 @@ class Person(Base):
     age = sqlalchemy.Column(sqlalchemy.Integer)
     #currently assuming 0-1 ranking for health measures
     #the data type may change laters
-    eye_health = sqlalchemy.Column(sqlalchemy.Float)
+    health = sqlalchemy.Column(sqlalchemy.Float)
     perceived_health = sqlalchemy.Column(sqlalchemy.Float)
     district = sqlalchemy.Column(sqlalchemy.String, index=True)
     state = sqlalchemy.Column(sqlalchemy.String)
     #urban or rural
     classification = sqlalchemy.Column(sqlalchemy.String)
 
-    def __init__(self, money, gender, age, eye_health, perceived_health, district, state,
+    def __init__(self, money, gender, age, health, perceived_health, district, state,
             classification):
         self.money = money
         self.gender = gender
         self.age = age
-        self.eye_health = eye_health
+        self.health = health
         self.perceived_health = perceived_health
         self.district = district
         self.state = state
@@ -153,14 +153,14 @@ class Person(Base):
         return {'money': self.money, 
                 'gender': self.gender,
                 'age': self.age,
-                'eye_health': self.eye_health,
+                'health': self.health,
                 'perceived_health': self.perceived_health,
                 'district': self.district,
                 'state': self.state,
                 'classification': self.classification}
 
     def __repr__(self):
-        return 'Person({0}, {1}, {2})'.format(self.money, self.eye_health, self.perceived_health)
+        return 'Person({0}, {1}, {2})'.format(self.money, self.health, self.perceived_health)
 
 class Database(object):
 
